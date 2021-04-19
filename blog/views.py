2477eb9f599e4075e66337cb4b9750444c5fc399
hurmuzrafael <78491225+hurmuzrafael@users.noreply.github.com>
2021-04-19 13:46:33 +0300

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
+from .models import Post
+
 
 
 # Create your views here.
@@ -28,7 +29,7 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 5
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
@@ -76,3 +77,5 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 def about(request):
     return render(request,'blog/about.html',{'title': 'About'})
+
+
